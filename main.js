957ee0e09d52457a4d33e5b6e70c65eeac807ee1
Lucas Falcão Artacho <falcao.artacho@gmail.com>
2018-11-27 20:01:14 -0200

    add motors to animation test

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ var Motor = require('./libs/motor');
 motor = new Motor("1");
 motor_2 = new Motor("2");
 
+motorA1 = new Motor("A1");
+motorA2 = new Motor("A2");
+motorA3 = new Motor("A3");
+
+motorB1 = new Motor("B1");
+motorB2 = new Motor("B2");
+motorB3 = new Motor("B3");
+
+motorC1 = new Motor("C1");
+motorC2 = new Motor("C2");
+motorC3 = new Motor("C3");
+
+var motors = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3",];
+
+
 counter = 0;
 
 step = 1;
@@ -40,8 +55,10 @@ function animate() {
     motor.sendCommand(counter);
 
     setTimeout(animate, counter);
-    }
     */
+
+    }
+
 
 animate();
 
