a16977ce79ef6636f7e764f8b64a03bac14c8c19
Jonathan Querubina <jotadev@me.com>
2018-11-27 01:13:43 -0200

    Update animation

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ motor = new Motor("1");
 
 counter = 0;
 
+step = 1;
+
 function animate() {
+
+    counter = counter + step;
+
+    if (counter == 100) {
+        step = -1;
+    }
+
+    if(counter == 0) {
+        step = 1
+    }
+
     motor.sendCommand(counter);
-    counter++;
+
     setTimeout(animate, counter);
 }
 
