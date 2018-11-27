41f40607ac3f0a98364060c94e31b7b64d4a5a04
Jonathan Querubina <jotadev@me.com>
2018-11-27 01:06:45 -0200

    Update motor structure

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ var Motor = require('./libs/motor');
 
 motor = new Motor("1");
 
-motor.sendCommand("1");
+counter = 0;
 
+function animate() {
+    motor.sendCommand(counter);
+    counter++;
+    setTimeout(animate, counter);
+}
+
+animate();
