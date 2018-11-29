184ecca9f65e24161c1ab26820685b12e2bfdcd4
Jonathan Querubina <jotadev@me.com>
2018-11-29 15:23:09 -0200

    base front-end

--- public/js/lib/internal/global.js
+++ public/js/lib/internal/global.js
@@ -1,10 +1,32 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var lastCommand = Date.now();
-
 var socket = io();
 socket
-    .on('command', function (command) {
-        lastCommand = Date.now();
-        // here we animate the web
+    .on('exec', function(command) {
+
+        // comandos traduzidos
+
+        var commands = {
+            "1_PASSO": 1,
+            "-1_PASSO": -1,
+            "5_PASSO": 5,
+            "-5_PASSO": -5,
+            "CLEAR_LIMITS": 0,
+            "SET_MAX": 99,
+            "SET_MIN": -99
+        }
+
+        var name = command.motor;
+        var cmd = command.command;
+        var current_motor = $("#" + command.motor);
+
+        if (cmd == "CLEAR_LIMITS") {
+            current_motor.css({ "marginTop": 0 + "px" });
+        } else if (cmd == "SET_MAX") {
+
+        } else if (cmd == "SET_MIN") {
+
+        } else {
+            var current_height = parseInt(current_motor.css("marginTop").replace("px", ""));
+            var new_height = current_height + commands[cmd];
+            current_motor.css({ "marginTop": new_height + "px" });
+        }
     });
