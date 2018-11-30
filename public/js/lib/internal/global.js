7c673f906a128603ec5b9350d86a7c862a766067
Lucas Falcão Artacho <falcao.artacho@gmail.com>
2018-11-30 16:29:28 -0200

    add animate on global.js

--- public/js/lib/internal/global.js
+++ public/js/lib/internal/global.js
@@ -2,17 +2,6 @@ var socket = io();
 socket
     .on('exec', function(command) {
 
-        // comandos traduzidos
-        // for(var i in name) {
-        //     if(Array.isArray(name)) {
-
-        //     } else {
-        //     // Do another thing
-        //     }
-        // }
-
-
-
         var name = command.motor;
         var cmd = command.command;
         var current_motor = $("#" + command.motor);
@@ -43,8 +32,12 @@ socket
             current_height = cmd;
         }
 
-        current_motor.height(current_height);
-
+        // current_motor.height(current_height);
+        current_motor.animate({
+            height: current_height
+          }, 100, function() {
+            // Animation complete.
+          });
 
 
         console.log(name);
